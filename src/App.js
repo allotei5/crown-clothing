419f4aa65a31646fc0f768d8b9a6f0511c3d4594
allotei <allotei5@gmail.com>
2022-02-23 16:59:16 +0000

    worked on collection preview components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './components/pages/homepage/homepage.component';
+import ShopPage from './components/pages/shop/shop.component';
+
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route path='/shop' component={ShopPage} />
+    </Switch>
   );
 }
 

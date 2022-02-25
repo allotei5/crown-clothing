6aa2451a84901c5798f078fd5e1319f8feed6914
allotei <allotei5@gmail.com>
2022-02-25 10:32:45 +0000

    Added header component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './components/pages/homepage/homepage.component';
 import ShopPage from './components/pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 
 function App() {
   return (
-    <Switch>
-      <Route exact path='/' component={HomePage} />
-      <Route path='/shop' component={ShopPage} />
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
+    </div>
   );
 }
 
